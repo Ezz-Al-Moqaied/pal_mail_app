42e3e440ccb331e7702df503f838878f421da6ab
YazanMaghary <zezo2537112@gmail.com>
2023-08-28 21:45:40 +0300

    Registration screen ui

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,6 +1,8 @@
 class Images {
   Images._();
-
   static const String appLogo = 'assets/images/appIcon.png';
-
+  static const String ellipse = 'assets/images/ellipse 1.png';
+  static const String faceBook = 'assets/images/facebook.png';
+  static const String twitter = 'assets/images/twitter.png';
+  static const String google = 'assets/images/google.png';
 }
---
+++ lib/constants/widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const smallSpacer = SizedBox(
+  height: 12,
+);
+const mediumSpacer = SizedBox(
+  height: 24,
+);
+const largeSpacer = SizedBox(
+  height: 48,
+);
