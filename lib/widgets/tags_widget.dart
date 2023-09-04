bacbb46a3ae21d65ed1bb919af8e3515a1137c22
EzzAlMoqaied <ezzalmoqaied2000@gmail.com>
2023-09-04 15:22:58 +0300

    The work is finished file Tage Widget ...

--- lib/widgets/tags_widget.dart
+++ lib/widgets/tags_widget.dart
@@ -1,48 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pal_mail_app/models/tage_model.dart';
 
 import '../constants/colors.dart';
 
 class TagsWidget extends StatelessWidget {
-  const TagsWidget({
+  List<String> tag;
+
+  TagsWidget({
     super.key,
+    required this.tag,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 100.h,
-        decoration: BoxDecoration(
-            color: colorWhite, borderRadius: BorderRadius.circular(30.r)),
-        child: GridView.builder(
-          padding: const EdgeInsets.all(16),
-          scrollDirection: Axis.vertical,
-          itemCount: 4,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              childAspectRatio: 3,
-              crossAxisCount: 3,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8),
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                print("Test");
-              },
-              child: Container(
-                alignment: Alignment.center,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                decoration: BoxDecoration(
-                    color: tagButtonColor,
-                    borderRadius:
-                        BorderRadiusDirectional.all(Radius.circular(30.r))),
-                child: Text(
-                  "#Urhent",
-                  style: TextStyle(color: tagTextColor, fontSize: 14.sp),
-                ),
-              ),
-            );
-          },
-        ));
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: tag.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 3,
+          crossAxisCount: 3,
+          mainAxisSpacing: 4.h,
+          crossAxisSpacing: 4.w),
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.all(4),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.r),
+              color: tagButtonColor
+            ),
+            child: Text("#${tag[index]}" , style: TextStyle(
+              color: tagTextColor ,
+              fontSize: 16.sp ,
+            ),));
+      },
+    );
   }
 }
+
+/*
+Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            color: tagButtonColor,
+            borderRadius: BorderRadius.all(Radius.circular(30.r)),
+          ),
+          child: TextButton(
+            child: Text(
+              tag[index].name!,
+              style: TextStyle(color: Colors.black, fontSize: 16.sp),
+            ),
+            onPressed: () {
+
+            },
+
+          ),
+        )
+ */
